252d77d92267f2fa177d79a4e399dbf193c0b3a9
Puneet Karamchandani <jkaramchandani32@gmail.com>
2022-11-07 22:41:11 +0530

    added ui framework

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react'
 
 import App from './App';
 
@@ -9,6 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-        <App/>
+        <ChakraProvider>
+          <App/>
+        </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>);
