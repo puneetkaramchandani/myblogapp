a57b4b06a733eaa5a8f776ef7da2c73c750f3ed5
Puneet Karamchandani <jkaramchandani32@gmail.com>
2022-11-10 10:08:06 +0530

    added bootstrap for ui  and fontawesome free for icons

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 
+import "@fortawesome/fontawesome-free/js/brands.js";
+import "@fortawesome/fontawesome-free/js/solid.js";
+import "@fortawesome/fontawesome-free/js/fontawesome.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
