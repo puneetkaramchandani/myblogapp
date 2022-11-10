b8056049fc23eff53555d1a04a8d35470d1224e4
Puneet Karamchandani <jkaramchandani32@gmail.com>
2022-11-10 11:52:47 +0530

    className warnings removed

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -3,8 +3,8 @@ import React from "react";
 const Loader = () => {
   return (
     <div className="container page d-flex justify-content-center align-items-center">
-      <div class="spinner-border text-warning" role="status">
-        <span class="visually-hidden">Loading...</span>
+      <div className="spinner-border text-warning" role="status">
+        <span className="visually-hidden">Loading...</span>
       </div>
     </div>
   );
