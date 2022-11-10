b86475bdf8139e43ee51bb55984c574a03036028
Puneet Karamchandani <jkaramchandani32@gmail.com>
2022-11-10 17:29:37 +0530

    Post Page version 1 completed

--- src/Pages/Post.js
+++ src/Pages/Post.js
@@ -1,10 +1,11 @@
 import { isEmpty } from "lodash";
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import DataLoadFailed from "../Components/DataLoadFailed";
 import Loader from "../Components/Loader";
 import { getPostDetailByPostId } from "../Utils/dummy_apis";
+import PostDetails from "../Views/PostDetails";
 
 const Post = () => {
   const { post_id } = useParams();
@@ -15,7 +16,7 @@ const Post = () => {
     getPostDetailByPostId(post_id)
       .then(async (res) => {
         const data = await res.json();
-        if(res.ok){
+        if (res.ok) {
           setPostDetails(data);
         } else {
           toast.error(`Error - ${data.error}`);
@@ -29,11 +30,19 @@ const Post = () => {
   }, []);
 
   return (
-    <main className="page d-flex justify-content-center align-items-center">
-      {isEmpty(postDetails) && loading && <Loader/>}
-      {isEmpty(postDetails) && !loading && <DataLoadFailed message={'Failed to load post details, something went wrong.'}/>}
-      {!isEmpty(postDetails) && !loading && <h1>Post Page - {post_id}</h1>}
-    </main>
+    <Fragment>
+      <main className="page d-flex justify-content-center align-items-center">
+        {isEmpty(postDetails) && loading && <Loader />}
+        {isEmpty(postDetails) && !loading && (
+          <DataLoadFailed
+            message={"Failed to load post details, something went wrong."}
+          />
+        )}
+        {!isEmpty(postDetails) && !loading && (
+          <PostDetails postDetails={postDetails} />
+        )}
+      </main>
+    </Fragment>
   );
 };
 
---
+++ src/Views/PostDetails.js
@@ -0,0 +1,58 @@
+import React from "react";
+import PropTypes from "prop-types";
+import moment from "moment";
+
+const PostDetails = ({ postDetails }) => {
+  
+  return (
+    <div className="row p-2 p-lg-5 font-monospace g-2 g-md-3 justify-content-evenly">
+      <div className="col-md-4">
+        <img className="img-fluid rounded-2" src={postDetails.image} />
+      </div>
+      <div className="col-md-6">
+        <div className="my-2">
+          {postDetails.tags.map((tag) => (
+            <span className="badge fs-5 bg-primary me-1">{tag}</span>
+          ))}
+        </div>
+        <div className="mt-4 fs-5">
+          <img
+            height={50}
+            width={50}
+            className="me-2 border rounded-circle"
+            src={postDetails.owner.picture}
+            alt={postDetails.owner.firstName + postDetails.owner.lastName}
+          />
+          {"Author:" +
+            " " +
+            postDetails.owner.title.toUpperCase() +
+            "." +
+            " " +
+            postDetails.owner.firstName +
+            " " +
+            postDetails.owner.lastName +
+            " , " +
+            "Posted At:" +
+            moment(postDetails.publishDate).format("LLL")}
+        </div>
+        <div className="mt-4 fs-4">
+          Likes - <i class="fa-solid fa-heart text-danger"></i> {postDetails.likes}
+        </div>
+        <div className="mt-4">
+          <textarea
+            style={{ resize: "none" }}
+            className="form-control"
+            readOnly={true}
+            value={postDetails.text}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+PostDetails.propTypes = {
+  postDetails: PropTypes.object,
+};
+
+export default PostDetails;
